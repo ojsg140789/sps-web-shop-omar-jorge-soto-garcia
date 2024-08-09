10b713ebf53797000d9286e33646225fc4a95631
Omar Soto <ojsg140789@gmail.com>
2024-08-08 20:22:54 -0600

    feature: Se agregan rutas

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,39 @@
 import { Routes } from '@angular/router';
+import { NoPageFoundComponent } from './pages/no-page-found/no-page-found.component';
 
-export const routes: Routes = [];
+
+
+export const routes: Routes = [
+    {
+        path: 'login',
+        loadComponent: () => 
+            import('./auth/login/login.component')
+                .then(m => m.LoginComponent)
+    },
+    {
+        path: 'register',
+        loadComponent: () => 
+            import('./auth/register/register.component')
+                .then(m => m.RegisterComponent)
+    },
+    {
+        path: 'dashboard',
+        loadComponent: () => 
+            import('./pages/dashboard/dashboard.component')
+                .then(m => m.DashboardComponent)
+    },
+    {
+        path: 'detail/:id',
+        loadComponent: () => 
+            import('./pages/product-detail/product-detail.component')
+                .then(m => m.ProductDetailComponent)
+    },
+    {
+        path: 'cart',
+        loadComponent: () => 
+            import('./pages/cart/cart.component')
+                .then(m => m.CartComponent)
+    },
+    { path: '',   redirectTo: '/dashboard', pathMatch: 'full' },
+    { path: '**', component: NoPageFoundComponent }
+];
