674371c2caad605b980e6632d82758641092cb86
Omar Soto <ojsg140789@gmail.com>
2024-08-09 17:31:21 -0600

    feature: Se agrega estilo a header y footer

--- src/app/shared/header/header.component.ts
+++ src/app/shared/header/header.component.ts
@@ -1,12 +1,19 @@
 import { Component } from '@angular/core';
+import { FontAwesomeModule } from '@fortawesome/angular-fontawesome';
+import { faUser, faCartShopping } from '@fortawesome/free-solid-svg-icons';
 
 @Component({
   selector: 'app-header',
   standalone: true,
-  imports: [],
+  imports: [FontAwesomeModule],
   templateUrl: './header.component.html',
   styleUrl: './header.component.css'
 })
 export class HeaderComponent {
+  faUser = faUser;
+  faCartShopping = faCartShopping
 
+  logout() {
+    
+  }
 }
