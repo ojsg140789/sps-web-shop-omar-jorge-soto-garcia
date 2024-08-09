fca0c62b89fb6c88fc3b03132d836b60c41b470f
Omar Soto <ojsg140789@gmail.com>
2024-08-08 19:02:20 -0600

    feature: Se agrega configuracion de firebase

--- src/app/app.config.ts
+++ src/app/app.config.ts
@@ -1,8 +1,20 @@
-import { ApplicationConfig, provideZoneChangeDetection } from '@angular/core';
+import { ApplicationConfig, importProvidersFrom, provideZoneChangeDetection } from '@angular/core';
 import { provideRouter } from '@angular/router';
 
 import { routes } from './app.routes';
 
+import { provideFirebaseApp, initializeApp } from '@angular/fire/app';
+import { getAuth, provideAuth } from '@angular/fire/auth'; 
+import { provideFirestore, getFirestore } from '@angular/fire/firestore';
+import { provideStorage, getStorage } from '@angular/fire/storage';
+import { environment } from '../environments/environment';
+
 export const appConfig: ApplicationConfig = {
-  providers: [provideZoneChangeDetection({ eventCoalescing: true }), provideRouter(routes)]
+  providers: [provideZoneChangeDetection({ eventCoalescing: true }), 
+    provideRouter(routes),
+    provideFirebaseApp(() => initializeApp(environment.firebase)),
+    provideAuth(() => getAuth()),
+    provideFirestore(() => getFirestore()),
+    provideStorage(() => getStorage())
+  ]
 };
---
+++ src/environments/environment.development.ts
@@ -0,0 +1,11 @@
+export const environment = {
+    firebase: {
+        apiKey: "AIzaSyAYQg66IF_pqHTMw1M1k6AVLxM9Mze6yxI",
+        authDomain: "sps-web-shop.firebaseapp.com",
+        projectId: "sps-web-shop",
+        storageBucket: "sps-web-shop.appspot.com",
+        messagingSenderId: "820970559076",
+        appId: "1:820970559076:web:10abcc2f93c73864f67171",
+        measurementId: "G-LQBSRQC26Y"
+    }
+};
