3576bced961631b3e3850980602998b74b3ab7e4
Omar Soto <ojsg140789@gmail.com>
2024-08-12 08:29:11 -0600

    feature: Se agrega prueba con cypress

--- cypress/e2e/test.cy.ts
+++ cypress/e2e/test.cy.ts
@@ -1,9 +1,25 @@
-describe('Primer', () => {
-    beforeEach('Open', () => {
-        cy.visit('http://localhost:4200');
+describe('Login Component', () => {
+    beforeEach(() => {
+      cy.visit('http://localhost:4200/login');
     });
-
-    it('Welcome', () => {
-        cy.contains('Bienvenido');
+  
+    it('Debe aparecer el formulario de login', () => {
+      cy.get('#validationCustomEmail').should('be.visible');
+      cy.get('#validationCustomPassword').should('be.visible');
+      cy.get('.btn-secondary', { timeout: 10000 }).eq(0).should('be.visible');
     });
-});
+  
+    it('Debe mostrar mensaje de credenciales erroneas', () => {
+      cy.get('#validationCustomEmail').type('invaliduser@gmail.com');
+      cy.get('#validationCustomPassword').type('wrongpassword');
+      cy.get('.btn-secondary', { timeout: 10000 }).eq(0).click();
+      cy.get('.swal2-title').should('contain', 'El correo y/o contraseña incorrectos');
+    });
+  
+    it('Debe mostrar Dashboard con login exitoso', () => {
+      cy.get('#validationCustomEmail').type('bar@gmail.com');
+      cy.get('#validationCustomPassword').type('1234567890');
+      cy.get('.btn-secondary', { timeout: 10000 }).eq(0).click();
+      cy.url().should('include', '/dashboard');
+    });
+  });
